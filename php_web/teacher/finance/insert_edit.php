f57b874dc5a27a0c9e7f4056936820e68b312ccf
konekohip <konekohip@tuta.io>
2025-12-09 23:19:21 +0800

    將insert,edit,delete替換成函式執行

--- php_web/teacher/finance/insert_edit.php
+++ php_web/teacher/finance/insert_edit.php
@@ -1,31 +1,5 @@
 <?php
-//能不能寫得像insert_expense一樣?
-
-$dsn="mysql:host=localhost;dbname=finance_db;charset=utf8";
-$pdo=new PDO($dsn,'root','');
-
-
-$sql="UPDATE `daily_account` SET ";
-    $tmp=[];
-    foreach($_POST as $key => $value){
-        $tmp[]="`$key`='$value'";
-    }
-$sql .= join(", ",$tmp);
-        /* ``='{}',
-        `time`='{$_POST['time']}',
-        `currency`='{$_POST['currency']}',
-        `item`='{$_POST['item']}',
-        `store`='{$_POST['store']}',
-        `category`='{$_POST['category']}',
-        `amount`='{$_POST['amount']}',
-        `payment_method`='{$_POST['payment_method']}',
-        `type`='{$_POST['type']}',
-        `account`='{$_POST['account']}',
-        `note`='{$_POST['note']}' */
-
-$sql .=" WHERE `id`='{$_POST['id']}'";
-
-echo $sql;
+update('daily_account',$_POST);
 $pdo->exec($sql);
 // header("location:index.php");
 ?>
