8fe14f7c7f99902c340e172bf01de7362c515890
shang <konekohip@tuta.io>
2025-12-17 16:37:15 +0800

    學習API使用方法

---
+++ web_api/api/get_menus.php
@@ -0,0 +1,9 @@
+<?php
+$menus = [
+    "回首頁" => "http://localhost/webapi.html/",
+    "更多消息" => "http://localhost/more_news.html/",
+    "校園映像" => "http://localhost/images.html/"
+];
+
+echo json_encode($menus);
+?>
---
+++ web_api/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB_API</title>
+</head>
+<body>
+    <h3>簡易Web Api</h3>
+    <a href="webapi.html">前往</a>
+</body>
+</html>
